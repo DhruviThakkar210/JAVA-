1874d7ed790c1770c9733599b1ff3cc7e41567a5
DhruviThakkar210 <103808644+DhruviThakkar210@users.noreply.github.com>
2022-11-14 23:44:53 +0530

    Add files via upload

---
+++ kunal kushwaha/Switch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+class Switch
+{
+
+
+
+public static void main(String[] args)
+ {
+
+ 	Scanner sc = new Scanner(System.in);
+ 	String name = sc.next();
+
+ 	switch(name)
+ 	{
+ 	case "Dhruvi":
+ 		System.out.println("MY FAV!!!");
+ 		break;
+ 	case "Vimarsh" :
+         System.out.println("HATRED HATRED!!!!!!!");
+         break;
+     default :
+     	System.out.println("ENTER VALID NAME");
+ 	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+}
